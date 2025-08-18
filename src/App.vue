f3fdfe5ce638116c068b34e2a10a36459d378c38
Tianyang <thuu0017@student.monash.edu>
2025-08-18 17:01:32 +1000

    feat(form): add Form.vue skeleton and mount in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
-  <div id="app">
-    <JSONLab />
-  </div>
+  <Form />
 </template>
 
 <script setup>
-import JSONLab from './components/JSON.vue'
+import Form from './components/Form.vue'
+// import JSONLab from './components/JSON.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 40px;
-}
-</style>
